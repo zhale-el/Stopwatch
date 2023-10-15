555fba658b522d25e86d06ca37a59dd2ff20b9a4
zhaleh <zhalehelmi@gmail.com>
2023-10-15 15:29:06 +0330

    start js

---
+++ script.js
@@ -0,0 +1,20 @@
+const timerElem = document.getElementById("timer");
+const startBtnElem = document.getElementById("start");
+const stopBtnElem = document.getElementById("stop");
+const resetBtnElem = document.getElementById("reset");
+
+function startTimer() {
+  //   console.log("start");
+}
+
+function stopTimer() {
+  //   console.log("stop");
+}
+
+function resetTimer() {
+  //   console.log("reset");
+}
+
+startBtnElem.addEventListener("click", startTimer);
+stopBtnElem.addEventListener("click", stopTimer);
+resetBtnElem.addEventListener("click", resetTimer);
